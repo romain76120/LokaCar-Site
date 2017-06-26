c3a534c8910ff4d98d14ec953af82134db0f2248
pbontempi2017 <philippe.bontempi2017@campus-eni.fr>
2017-06-26 14:56:47 +0200

    Push 2: - ajout des beans - ajout des permissions

---
+++ app/src/main/java/eni_ecole/fr/lokacarsite/beans/Client.java
@@ -0,0 +1,9 @@
+package eni_ecole.fr.lokacarsite.beans;
+
+/**
+ * Created by pbontempi2017 on 26/06/2017.
+ */
+
+public class Client {
+
+}
---
+++ app/src/main/java/eni_ecole/fr/lokacarsite/beans/Leasing.java
@@ -0,0 +1,8 @@
+package eni_ecole.fr.lokacarsite.beans;
+
+/**
+ * Created by pbontempi2017 on 26/06/2017.
+ */
+
+public class Leasing {
+}
